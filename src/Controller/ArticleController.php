fff09542f9de6d6803b06acd7aaacbf7fa0d5371
Igor de Medeiros <igormedeiros0@gmail.com>
2019-01-10 06:45:11 -0300

    add annotations and first controller actions/routes

---
+++ src/Controller/ArticleController.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: igor
+ * Date: 10/01/19
+ * Time: 06:35
+ */
+
+namespace App\Controller;
+
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\HttpFoundation\Response;
+
+class ArticleController
+{
+    /**
+     * @Route("/")
+     */
+    public function homepage()
+    {
+        return new Response('OMG! My first page already! WOOO!');
+    }
+
+    /**
+     * @Route("/news/{slug}")
+     */
+    public function show($slug)
+    {
+        return new Response(sprintf(
+            'Future page to show the article: "%s"',
+            $slug
+        ));
+    }
+}
